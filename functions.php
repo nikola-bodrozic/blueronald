843884e892ddb7eb94495a74d6728e98a08adda6
Nikola Bodrozic <nb@example.com>
2014-10-02 00:50:04 +0200

    added pagination for search page

--- functions.php
+++ functions.php
@@ -6,9 +6,17 @@ register_nav_menus( array(
     'secondary' => __( 'Menu 2', 'blueronald' ),    
 ) );
 
+add_action( 'pre_get_posts', 'blueronald_posts_per_pare_setup' );
 
-add_action('after_setup_theme', 'my_theme_setup');
-function my_theme_setup(){
+function blueronald_posts_per_pare_setup(){
+    global $wp_query;
+	$wp_query->set('posts_per_page', 5);
+}
+
+
+add_action('after_setup_theme', 'blueronald_lang_setup');
+
+function blueronald_lang_setup(){
     load_theme_textdomain('blueronald', get_template_directory() . '/languages');
 }
 
@@ -36,4 +44,21 @@ function theme_slug_widgets_init() {
     ) );
 }
 add_action( 'widgets_init', 'theme_slug_widgets_init' );
+
+function blueronald_paging_navigation() {
+	if ( $GLOBALS['wp_query']->max_num_pages < 2 ) {
+		return;
+	}
+	?>
+		<div class="navigation">
+			<?php if ( get_next_posts_link() ) : ?>
+			<div class="alignleft" style="display: inline;"> <?php next_posts_link('<button type="button" class="btn btn-default">'.__('&raquo; Older Posts ','blueronald').'</button>'); ?></div>
+			<?php endif; ?>
+
+			<?php if ( get_previous_posts_link() ) : ?>
+			<div class="alignright" style="display: inline;"><?php previous_posts_link('<button type="button" class="btn btn-default">'.__(' Newer Posts &laquo;','blueronald').'</button>'); ?> </div>
+			<?php endif; ?>
+		</div><!-- .nav-links -->
+	<?php
+}
 ?>
--- search.php
+++ search.php
@@ -1,23 +1,29 @@
 <?php get_header(); ?>
 <div class="row">
-	<div class="col-md-8">	
-<?php
-$rec = esc_attr($_GET['s']);
-$query = new WP_Query( array(
-    'post_type' => array('post', 'page'),
-    's' => $rec,
-    'posts_per_page' => 2,
-));
-while ( $query->have_posts() ) {     $query->the_post();
-?>
+	<div class="col-md-8">
+		<?php if ( have_posts() ) : ?>
+
+				<h1><?php printf( __( 'Search Results for: %s', 'blueronald' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
+
+			<?php while ( have_posts() ) : the_post(); ?>
+
+
 				<h3><?php the_title(); ?></h3>
-				<?php the_excerpt(); ?> 
+				
+				<?php the_excerpt(); ?>
+				
 				<p><a class="btn btn-primary btn-lg" role="button" href="<?php the_permalink(); ?>"><?php _e('Read More...', 'blueronald'); ?></a></p>
-<?php } ?>
 
+			<?php endwhile; ?>
+
+			<?php echo "<br>"; blueronald_paging_navigation(); echo "<br>"; ?>
+
+		<?php else : ?>
+
+			<?php echo "string"; ?>
+
+		<?php endif; ?>
 	</div>
-
-		<?php get_sidebar('right'); ?>
+	<?php get_sidebar('right'); ?>
 </div>
-
 <?php get_footer(); ?>
