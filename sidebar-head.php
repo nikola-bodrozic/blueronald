80c9aa6338e923df39cd90bea850da934e73fbe1
Nikola Bodrozic <nb@example.com>
2014-10-04 18:26:14 +0200

    deleted  unnecessary file sidebar-head.php

--- sidebar-head.php
+++
@@ -1,5 +0,0 @@
-			    <div class="col-md-4">					
-					<?php if ( is_active_sidebar( 'in-header-widget-area' ) ) { ?>
-					    <?php dynamic_sidebar( 'in-header-widget-area' ); ?>
-					<?php } ?>
-				</div>
